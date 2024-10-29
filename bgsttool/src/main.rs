54221df3238e6a82325e1396e71ddaaa5d7b9b25
Swift <swiftshineexists@gmail.com>
2024-10-29 18:53:50 -0500

    refactor, add wip masking

--- bgsttool/src/main.rs
+++ bgsttool/src/main.rs
@@ -1,16 +1,23 @@
 use std::env;
 use anyhow::{Result, bail};
+use bgst;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
+    if args.len() < 2 {
         bail!("incorrect argument count");
     }
 
     let filename = &args[1];
+    
+    let mut should_mask = false;
 
-    let result = bgst::bgst_processing::extract_bgst(&filename);
+    if args.len() == 3 {
+        should_mask = &args[2] == "mask";
+    }
+
+    let result = bgst::extract_bgst(&filename, should_mask);
 
     result
 }
